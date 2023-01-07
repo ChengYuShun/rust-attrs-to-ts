fc274c6a8a89bb2914a023b6e27ca7d33cf35361
Yushun Cheng <chengys@disroot.org>
2023-01-07 11:35:45 +0800

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use proc_macro2::TokenStream as TokenStream2;
+
+use syn::{
+    parenthesized,
+    parse::{Parse, ParseStream, Result as SynResult},
+    parse2, Attribute,
+};
+
+struct Ts(TokenStream2);
+
+impl Parse for Ts {
+    fn parse(input: ParseStream) -> SynResult<Self> {
+        let content;
+        parenthesized!(content in input);
+        Ok(Self(content.parse()?))
+    }
+}
+
+pub fn attrs_to_ts(attrs: &[Attribute], name: &str) -> Option<TokenStream2> {
+    attrs
+        .iter()
+        .filter(|attr| attr.path.segments.len() == 1)
+        .filter(|attr| attr.path.segments[0].ident == name)
+        .last()
+        .map(|attr| {
+            parse2::<Ts>(attr.tokens.clone())
+                .expect("invalid parsed attributes")
+                .0
+        })
+}
